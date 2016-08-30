374ba2256ac53b81958fc3b9f5b594d34780ca12
LeafyDev <will09600@gmail.com>
2016-08-30 17:36:03 +0200

    Added proper command handler

---
+++ Lyudmila.Server/CommandHandler.cs
@@ -0,0 +1,59 @@
+﻿// -----------------------------------------------------------
+// Copyrights (c) 2016 Seditio 🍂 INC. All rights reserved.
+// -----------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Lyudmila.Server
+{
+    public struct CommandRecord
+    {
+        public string commandName, helpText;
+        public CommandHost.CommandHandler handler;
+    }
+
+    public class CommandHost
+    {
+        public delegate void CommandHandler(string param, Dictionary<string, CommandRecord> commandMap);
+
+        public readonly Dictionary<string, CommandRecord> commandMap = new Dictionary<string, CommandRecord>();
+
+        public CommandHost()
+        {
+            RegisterCommand("help", "Show available commands", Commands.Help);
+            RegisterCommand("clear", "Clear the console display", Commands.Clear);
+        }
+
+        public void RegisterCommand(string commandName, string helpText, CommandHandler handler)
+        {
+            var record = new CommandRecord {commandName = commandName, helpText = helpText, handler = handler};
+            commandMap.Add(commandName, record);
+        }
+
+        public bool InvokeCommand(string inputLine)
+        {
+            inputLine = inputLine.TrimStart();
+
+            var commandNameLength = 0;
+            
+            for (; commandNameLength < inputLine.Length && inputLine[commandNameLength] != ' '; commandNameLength++) {}
+
+            var commandName = inputLine.Substring(0, commandNameLength);
+            var param = inputLine.Substring(commandNameLength).TrimStart();
+
+            if(commandName == "quit")
+            {
+                return false;
+            }
+
+            if(commandMap.ContainsKey(commandName))
+            {
+                commandMap[commandName].handler.Invoke(param, commandMap);
+                return true;
+            }
+
+            Logger.Write($"Unknown command {commandName}", LogLevel.Console);
+            return true;
+        }
+    }
+}
---
+++ Lyudmila.Server/Commands.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------
+// Copyrights (c) 2016 Seditio 🍂 INC. All rights reserved.
+// -----------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Lyudmila.Server
+{
+    internal class Commands
+    {
+        public static void Help(string param, Dictionary<string, CommandRecord> commandMap)
+        {
+            var helpText = "Available commands: \r\nquit: Exit the program";
+            foreach(var s in commandMap.Keys)
+            {
+                helpText += "\r\n" + s + ": ";
+                helpText += commandMap[s].helpText;
+            }
+            Logger.Write(helpText, LogLevel.Console);
+        }
+
+        public static void Clear(string param, Dictionary<string, CommandRecord> commandmap) => Console.Clear();
+    }
+}
