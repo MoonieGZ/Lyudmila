417068e15bcbef7e11898e792117eb68e0e272c7
LeafyDev <will09600@gmail.com>
2016-08-31 11:51:46 +0200

    Why are these here?

--- Lyudmila/Windows/MainWindow.xaml.cs
+++ Lyudmila/Windows/MainWindow.xaml.cs
@@ -4,9 +4,6 @@
 
 using System;
 using System.ComponentModel;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
