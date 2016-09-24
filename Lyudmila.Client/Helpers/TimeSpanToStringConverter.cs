32cce92cdda45235fec48bd6b6d6ad0f3ded288c
LeafyDev <will09600@gmail.com>
2016-09-25 00:27:10 +0200

    Moved stuff to nicer folders

--- Lyudmila.Client/Helpers/TimeSpanToStringConverter.cs
+++ Lyudmila.Client/Helpers/TimeSpanToStringConverter.cs
@@ -6,7 +6,7 @@ using System;
 using System.Globalization;
 using System.Windows.Data;
 
-namespace Lyudmila.Client.Resources.Helpers
+namespace Lyudmila.Client.Helpers
 {
     [ValueConversion(typeof(TimeSpan?), typeof(string))]
     internal class TimeSpanToStringConverter : IValueConverter
