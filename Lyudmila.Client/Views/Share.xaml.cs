7d0a0c6d2a94941ffb15ca29052dae89fb0fc159
LeafyDev <will09600@gmail.com>
2016-10-19 16:45:06 +0200

    Début de pages pour le partage de fichiers
    La fonction pour partager les fichiers restera avec une priorité assez
    basse sachant qu'on a deja moyen de faire autrement, c'est juste une
    idée pour regrouper le tout en un

---
+++ Lyudmila.Client/Views/Share.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lyudmila.Client.Views
+{
+    /// <summary>
+    /// Interaction logic for Share.xaml
+    /// </summary>
+    public partial class Share : UserControl
+    {
+        public Share()
+        {
+            InitializeComponent();
+        }
+    }
+}
