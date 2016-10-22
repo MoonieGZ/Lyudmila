75a8f4f2a6bf4a36bcc870d1ebd1f4a7b4e5b63a
LeafyDev <will09600@gmail.com>
2016-10-22 19:41:29 +0200

    Ajout fenetre téléchargement / extraction

---
+++ Lyudmila.Client/Windows/Download.xaml.cs
@@ -0,0 +1,127 @@
+﻿// -----------------------------------------------------------
+// Copyrights (c) 2016 Seditio 🍂 INC. All rights reserved.
+// -----------------------------------------------------------
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Lyudmila.Client.Properties;
+
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Readers;
+
+namespace Lyudmila.Client.Windows
+{
+    /// <summary>
+    ///   Interaction logic for Download.xaml
+    /// </summary>
+    public partial class Download : INotifyPropertyChanged
+    {
+        private string _headerText;
+
+        private readonly string _gameName;
+
+        public Download(string gameName, string url)
+        {
+            InitializeComponent();
+            HeaderText = $"Téléchargement: {gameName}";
+            _gameName = gameName;
+
+            if(File.Exists(Path.Combine(Environment.CurrentDirectory, "DL", url.Split('/').Last())))
+            {
+                new Thread(Extract).Start();
+            }
+            else
+            {
+                startDownload(url);
+            }
+        }
+
+        private void startDownload(string url)
+        {
+            Task.Delay(1000);
+            if(!Directory.Exists("DL"))
+            {
+                Directory.CreateDirectory("DL");
+            }
+
+            var thread = new Thread(() => {
+                var client = new WebClient();
+                client.DownloadProgressChanged += client_DownloadProgressChanged;
+                client.DownloadFileCompleted += client_DownloadFileCompleted;
+                client.DownloadFileAsync(new Uri(url), Path.Combine(Environment.CurrentDirectory, "DL", url.Split('/').Last()));
+            });
+            progressBar.IsIndeterminate = false;
+            thread.Start();
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Dispatcher.Invoke((MethodInvoker)delegate {
+                var bytesIn = double.Parse(e.BytesReceived.ToString());
+                var totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                var percentage = bytesIn / totalBytes * 100;
+                
+                progressBar.Value = int.Parse(Math.Truncate(percentage).ToString(CultureInfo.InvariantCulture));
+            });
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            new Thread(Extract).Start();
+        }
+
+        private void Extract()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                progressBar.IsIndeterminate = true;
+            });
+            
+            switch (_gameName)
+            {
+                case "AoE2HD":
+                    if (!Directory.Exists("Jeux"))
+                    {
+                        Directory.CreateDirectory("Jeux");
+                    }
+
+                    var zipPath = Path.Combine(Environment.CurrentDirectory, "DL", "AoE2HD.zip");
+                    var extractPath = Path.Combine(Environment.CurrentDirectory, "Jeux", "AoE2HD");
+
+                    using (var archive = ZipArchive.Open(zipPath))
+                    {
+                        foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
+                        {
+                            entry.WriteToDirectory(extractPath, new ExtractionOptions { ExtractFullPath = true, Overwrite = true });
+                        }
+                    }
+
+                    Settings.Default.AoE2HD_Installed = true;
+                    Settings.Default.AoE2HD_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "AoE2HD");
+                    break;
+            }
+            Dispatcher.Invoke(Close);
+        }
+
+        public string HeaderText
+        {
+            get { return _headerText; }
+            set
+            {
+                _headerText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("HeaderText"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
