1121814b5ffc5c988b5ac30062c0a1aabfd0a8b1
LeafyDev <will09600@gmail.com>
2016-10-22 18:10:08 +0200

    Ajout réglages jeux + localisations

--- Lyudmila.Client/Properties/Settings.Designer.cs
+++ Lyudmila.Client/Properties/Settings.Designer.cs
@@ -58,5 +58,413 @@ namespace Lyudmila.Client.Properties {
                 this["ServerIP"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool AoE2HD_Installed {
+            get {
+                return ((bool)(this["AoE2HD_Installed"]));
+            }
+            set {
+                this["AoE2HD_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool BF3_Installed {
+            get {
+                return ((bool)(this["BF3_Installed"]));
+            }
+            set {
+                this["BF3_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Blur_Installed {
+            get {
+                return ((bool)(this["Blur_Installed"]));
+            }
+            set {
+                this["Blur_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool CoD2_Installed {
+            get {
+                return ((bool)(this["CoD2_Installed"]));
+            }
+            set {
+                this["CoD2_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool CoD4_Installed {
+            get {
+                return ((bool)(this["CoD4_Installed"]));
+            }
+            set {
+                this["CoD4_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool CoD5_Installed {
+            get {
+                return ((bool)(this["CoD5_Installed"]));
+            }
+            set {
+                this["CoD5_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool CSGO_Installed {
+            get {
+                return ((bool)(this["CSGO_Installed"]));
+            }
+            set {
+                this["CSGO_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool DoDS_Installed {
+            get {
+                return ((bool)(this["DoDS_Installed"]));
+            }
+            set {
+                this["DoDS_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool DoTA2_Installed {
+            get {
+                return ((bool)(this["DoTA2_Installed"]));
+            }
+            set {
+                this["DoTA2_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool F2_Installed {
+            get {
+                return ((bool)(this["F2_Installed"]));
+            }
+            set {
+                this["F2_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Insurgency_Installed {
+            get {
+                return ((bool)(this["Insurgency_Installed"]));
+            }
+            set {
+                this["Insurgency_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool L4D2_Installed {
+            get {
+                return ((bool)(this["L4D2_Installed"]));
+            }
+            set {
+                this["L4D2_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool SC2_Installed {
+            get {
+                return ((bool)(this["SC2_Installed"]));
+            }
+            set {
+                this["SC2_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Shootmania_Installed {
+            get {
+                return ((bool)(this["Shootmania_Installed"]));
+            }
+            set {
+                this["Shootmania_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool SWJK2_Installed {
+            get {
+                return ((bool)(this["SWJK2_Installed"]));
+            }
+            set {
+                this["SWJK2_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool TF2_Installed {
+            get {
+                return ((bool)(this["TF2_Installed"]));
+            }
+            set {
+                this["TF2_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool UT3_Installed {
+            get {
+                return ((bool)(this["UT3_Installed"]));
+            }
+            set {
+                this["UT3_Installed"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string AoE2HD_Location {
+            get {
+                return ((string)(this["AoE2HD_Location"]));
+            }
+            set {
+                this["AoE2HD_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string BF3_Location {
+            get {
+                return ((string)(this["BF3_Location"]));
+            }
+            set {
+                this["BF3_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Blur_Location {
+            get {
+                return ((string)(this["Blur_Location"]));
+            }
+            set {
+                this["Blur_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string CoD2_Location {
+            get {
+                return ((string)(this["CoD2_Location"]));
+            }
+            set {
+                this["CoD2_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string CoD4_Location {
+            get {
+                return ((string)(this["CoD4_Location"]));
+            }
+            set {
+                this["CoD4_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string CoD5_Location {
+            get {
+                return ((string)(this["CoD5_Location"]));
+            }
+            set {
+                this["CoD5_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string CSGO_Location {
+            get {
+                return ((string)(this["CSGO_Location"]));
+            }
+            set {
+                this["CSGO_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string DoDS_Location {
+            get {
+                return ((string)(this["DoDS_Location"]));
+            }
+            set {
+                this["DoDS_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string DoTA2_Location {
+            get {
+                return ((string)(this["DoTA2_Location"]));
+            }
+            set {
+                this["DoTA2_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string F2_Location {
+            get {
+                return ((string)(this["F2_Location"]));
+            }
+            set {
+                this["F2_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Insurgency_Location {
+            get {
+                return ((string)(this["Insurgency_Location"]));
+            }
+            set {
+                this["Insurgency_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string L4D2_Location {
+            get {
+                return ((string)(this["L4D2_Location"]));
+            }
+            set {
+                this["L4D2_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string SC2_Location {
+            get {
+                return ((string)(this["SC2_Location"]));
+            }
+            set {
+                this["SC2_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Shootmania_Location {
+            get {
+                return ((string)(this["Shootmania_Location"]));
+            }
+            set {
+                this["Shootmania_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string SWJK2_Location {
+            get {
+                return ((string)(this["SWJK2_Location"]));
+            }
+            set {
+                this["SWJK2_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string TF2_Location {
+            get {
+                return ((string)(this["TF2_Location"]));
+            }
+            set {
+                this["TF2_Location"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string UT3_Location {
+            get {
+                return ((string)(this["UT3_Location"]));
+            }
+            set {
+                this["UT3_Location"] = value;
+            }
+        }
     }
 }
