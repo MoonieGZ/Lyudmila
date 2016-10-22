a3acf1e527605e8767185dfec7f291e59f83f496
LeafyDev <will09600@gmail.com>
2016-10-22 19:41:36 +0200

    MaJ pseudo

---
+++ Lyudmila.Client/Helpers/UsernameUpdater.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------
+// Copyrights (c) 2016 Seditio 🍂 INC. All rights reserved.
+// -----------------------------------------------------------
+
+using System.IO;
+
+using Lyudmila.Client.Properties;
+
+namespace Lyudmila.Client.Helpers
+{
+    internal class UsernameUpdater
+    {
+        public static void SetName(string gameName)
+        {
+            switch(gameName)
+            {
+                case "AoE2HD":
+                    var path = Path.Combine(Settings.Default.AoE2HD_Location, "SmartSteamEmu.ini");
+                    var SettingsFile = File.ReadAllLines(path);
+                    var i = 0;
+                    foreach (var line in SettingsFile)
+                    {
+                        if(line.StartsWith("PersonaName = "))
+                        {
+                            var newLine = line.Replace("Unreal", Settings.Default.Username);
+                            SettingsFile[i] = newLine;
+                            File.WriteAllLines(path, SettingsFile);
+                            return;
+                        }
+                        i++;
+                    }
+                    break;
+            }
+        }
+    }
+}
