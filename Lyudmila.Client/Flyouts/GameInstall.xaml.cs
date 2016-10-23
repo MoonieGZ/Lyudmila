0b33cca9a03a53167ae57eb32a7e48a28934fbe5
LeafyDev <will09600@gmail.com>
2016-10-23 02:02:26 +0200

    Ajout des jeux

--- Lyudmila.Client/Flyouts/GameInstall.xaml.cs
+++ Lyudmila.Client/Flyouts/GameInstall.xaml.cs
@@ -70,10 +70,40 @@ namespace Lyudmila.Client.Flyouts
                     switch (((MainWindow)Application.Current.MainWindow).GameInstall.Header)
                     {
                         case "Age of Empires II HD":
-                            new Download("AoE2HD", "http://192.168.0.252/jeux/AoE2HD.zip").ShowDialog();
+                            new Download("AoE2HD", $"http://{Properties.Settings.Default.ServerIP}/jeux/AoE2HD.zip").ShowDialog();
                             Properties.Settings.Default.AoE2HD_Installed = true;
                             Properties.Settings.Default.AoE2HD_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "AoE2HD");
                             UsernameUpdater.SetName("AoE2HD");
+                            break;
+                        case "Battlefield 3":
+                            new Download("BF3", $"http://{Properties.Settings.Default.ServerIP}/jeux/BF3.zip").ShowDialog();
+                            Properties.Settings.Default.BF3_Installed = true;
+                            Properties.Settings.Default.BF3_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "BF3");
+                            UsernameUpdater.SetName("BF3");
+                            break;
+                        case "Blur":
+                            new Download("Blur", $"http://{Properties.Settings.Default.ServerIP}/jeux/Blur.zip").ShowDialog();
+                            Properties.Settings.Default.Blur_Installed = true;
+                            Properties.Settings.Default.Blur_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "Blur");
+                            UsernameUpdater.SetName("Blur");
+                            break;
+                        case "CoD2":
+                            new Download("CoD2", $"http://{Properties.Settings.Default.ServerIP}/jeux/Blur.zip").ShowDialog();
+                            Properties.Settings.Default.CoD2_Installed = true;
+                            Properties.Settings.Default.CoD2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "CoD2");
+                            UsernameUpdater.SetName("CoD2");
+                            break;
+                        case "CoD4":
+                            new Download("CoD4", $"http://{Properties.Settings.Default.ServerIP}/jeux/CoD4.zip").ShowDialog();
+                            Properties.Settings.Default.CoD4_Installed = true;
+                            Properties.Settings.Default.CoD4_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "CoD4");
+                            UsernameUpdater.SetName("CoD4");
+                            break;
+                        case "CoD5":
+                            new Download("CoD5", $"http://{Properties.Settings.Default.ServerIP}/jeux/CoD5.zip").ShowDialog();
+                            Properties.Settings.Default.CoD5_Installed = true;
+                            Properties.Settings.Default.CoD5_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "CoD5");
+                            UsernameUpdater.SetName("CoD5");
                             break;
                     }
 
