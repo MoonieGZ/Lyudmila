a0e1c9cc03da011f6ed7ac2312801426c290edda
LeafyDev <will09600@gmail.com>
2016-10-24 04:11:47 +0200

    Auto update pret

--- Lyudmila.Client/App.xaml.cs
+++ Lyudmila.Client/App.xaml.cs
@@ -3,14 +3,17 @@
 // -----------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Net;
 using System.Reflection;
+using System.Security.Cryptography;
+using System.Threading;
 using System.Windows;
 
+using Lyudmila.Client.Properties;
 using Lyudmila.Client.Windows;
-
-using MaterialDesignThemes.Wpf;
 
 namespace Lyudmila.Client
 {
@@ -21,77 +24,84 @@ namespace Lyudmila.Client
     {
         private void StartApp(object sender, StartupEventArgs e)
         {
-            AppDomain.CurrentDomain.UnhandledException += delegate (object sender2, UnhandledExceptionEventArgs eargs)
+            AppDomain.CurrentDomain.UnhandledException += delegate(object sender2, UnhandledExceptionEventArgs eargs)
             {
-                var exception = (Exception)eargs.ExceptionObject;
+                var exception = (Exception) eargs.ExceptionObject;
 
-                if (MessageBox.Show($"Unhandled exception:{Environment.NewLine}{exception.GetType()}: {exception.Message}", "Lyudmila", MessageBoxButton.OK, MessageBoxImage.Error) == MessageBoxResult.OK)
+                if(MessageBox.Show($"Unhandled exception:{Environment.NewLine}{exception.GetType()}: {exception.Message}", "Lyudmila", MessageBoxButton.OK,
+                       MessageBoxImage.Error) == MessageBoxResult.OK)
                 {
                     Environment.Exit(1);
                 }
             };
 
-            if (File.Exists("easteregg.ini"))
+            if(e.Args.Length == 0)
             {
-                var color = string.Empty;
-                var accent = string.Empty;
-                var file = File.ReadAllLines("easteregg.ini");
-                if (file.Length.Equals(2)) // Color, Accent
-                {
-                    color = file[0];
-                    accent = file[1];
-                }
-                if (file.Length.Equals(1)) // Color
-                {
-                    color = file[0];
-                }
                 try
                 {
-                    var accentList = new List<string>();
-                    var colorList = new List<string>();
-
-                    var assembly = Assembly.GetExecutingAssembly();
-                    using (var stream = assembly.GetManifestResourceStream("Lyudmila.Client.Resources.Colors.txt"))
+                    var newHash = new WebClient().DownloadString(new Uri($"http://{Settings.Default.ServerIP}/logiciels/launcher/hash.md5")).ToLower();
+                    var currentHash = GetHash(Assembly.GetEntryAssembly().Location).ToLower();
+                    
+                    if(newHash.Equals(currentHash))
                     {
-                        if (stream != null)
-                            using (var reader = new StreamReader(stream))
+                        new MainWindow().Show();
+                    }
+                    else
+                    {
+                        var updating = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Lyudmila\\updating.vbs";
+                        using (
+                            var resource =
+                                Assembly.GetExecutingAssembly()
+                                    .GetManifestResourceStream("Lyudmila.Client.Resources.scripts.updating.vbs"))
+                        {
+                            using (var updater = new FileStream(updating, FileMode.Create, FileAccess.Write))
                             {
-                                while (reader.Peek() >= 0)
-                                {
-                                    colorList.Add(reader.ReadLine());
-                                }
+                                resource?.CopyTo(updater);
                             }
-                    }
-                    using (var stream = assembly.GetManifestResourceStream("Lyudmila.Client.Resources.Accents.txt"))
-                    {
-                        if (stream != null)
-                            using (var reader = new StreamReader(stream))
-                            {
-                                while (reader.Peek() >= 0)
-                                {
-                                    accentList.Add(reader.ReadLine());
-                                }
-                            }
-                    }
+                        }
+                        Process.Start(updating);
 
-                    if (!string.IsNullOrEmpty(color) && colorList.Contains(color))
-                    {
-                        new PaletteHelper().ReplacePrimaryColor(color);
-                    }
-
-                    if (!string.IsNullOrEmpty(accent) && accentList.Contains(accent))
-                    {
-                        new PaletteHelper().ReplaceAccentColor(accent);
+                        var me = Assembly.GetExecutingAssembly().Location;
+                        File.Copy(me, "Updating_Lyudmila.exe");
+                        Process.Start("Updating_Lyudmila.exe", "-update");
+                        Environment.Exit(0);
                     }
                 }
-                catch (Exception)
+                catch(Exception ex)
                 {
-                    MessageBox.Show("Easter bunny doesn't like you for some reason...");
-                    throw;
+                    MessageBox.Show($"{ex.GetType()}: {ex.Message}");
+                    Environment.Exit(1);
                 }
             }
+            else
+            {
+                if (e.Args.First() == "-update")
+                {
+                    Thread.Sleep(2000);
+                    File.Delete("Lyudmila.Client.exe");
+                    new WebClient().DownloadFile(new Uri($"http://{Settings.Default.ServerIP}/logiciels/launcher/Lyudmila.Client.exe"),
+                        @"Lyudmila.Client.exe");
+                    Process.Start("Lyudmila.Client.exe", "-run");
+                    Environment.Exit(0);
+                }
+                if (e.Args.First() == "-run")
+                {
+                    Thread.Sleep(2000);
+                    File.Delete("Updating_Lyudmila.exe");
+                    new MainWindow().Show();
+                }
+            }
+        }
 
-            new MainWindow().Show();
+        private static string GetHash(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                }
+            }
         }
     }
 }
---
+++ Lyudmila.Client/Resources/scripts/updating.vbs
@@ -0,0 +1,1 @@
+x=msgbox("Mise a jour de Lyudmila en cours...", 0+64, "Lyudmila")
