5420c079c833df62d74f05510065ae5dae6f3273
LeafyDev <will09600@gmail.com>
2016-10-25 19:38:56 +0200

    Revert "Revert "Ajouté l'extraction d'autres jeux""
    This reverts commit c69e4cfc7d745692a19359befc6b816c81d30cc6.

--- Lyudmila.Client/Windows/Download.xaml.cs
+++ Lyudmila.Client/Windows/Download.xaml.cs
@@ -25,9 +25,8 @@ namespace Lyudmila.Client.Windows
     /// </summary>
     public partial class Download : INotifyPropertyChanged
     {
-        private string _headerText;
-
         private readonly string _gameName;
+        private string _headerText;
 
         public Download(string gameName, string url)
         {
@@ -45,75 +44,6 @@ namespace Lyudmila.Client.Windows
             }
         }
 
-        private void startDownload(string url)
-        {
-            Task.Delay(1000);
-            if(!Directory.Exists("DL"))
-            {
-                Directory.CreateDirectory("DL");
-            }
-
-            var thread = new Thread(() => {
-                var client = new WebClient();
-                client.DownloadProgressChanged += client_DownloadProgressChanged;
-                client.DownloadFileCompleted += client_DownloadFileCompleted;
-                client.DownloadFileAsync(new Uri(url), Path.Combine(Environment.CurrentDirectory, "DL", url.Split('/').Last()));
-            });
-            progressBar.IsIndeterminate = false;
-            thread.Start();
-        }
-
-        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            Dispatcher.Invoke((MethodInvoker)delegate {
-                var bytesIn = double.Parse(e.BytesReceived.ToString());
-                var totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
-                var percentage = bytesIn / totalBytes * 100;
-                
-                progressBar.Value = int.Parse(Math.Truncate(percentage).ToString(CultureInfo.InvariantCulture));
-            });
-        }
-
-        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            new Thread(Extract).Start();
-        }
-
-        private void Extract()
-        {
-            Dispatcher.Invoke(() =>
-            {
-                progressBar.IsIndeterminate = true;
-            });
-
-            if (!Directory.Exists("Jeux"))
-            {
-                Directory.CreateDirectory("Jeux");
-            }
-
-            switch (_gameName) // TODO
-            {
-                case "AoE2HD":
-                    ExtractCmd(Path.Combine(Environment.CurrentDirectory, "DL", "AoE2HD.zip"), Path.Combine(Environment.CurrentDirectory, "Jeux", "AoE2HD"));
-
-                    Settings.Default.AoE2HD_Installed = true;
-                    Settings.Default.AoE2HD_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", "AoE2HD");
-                    break;
-            }
-            Dispatcher.Invoke(Close);
-        }
-
-        private static void ExtractCmd(string zipPath, string extractPath)
-        {
-            using (var archive = ZipArchive.Open(zipPath))
-            {
-                foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
-                {
-                    entry.WriteToDirectory(extractPath, new ExtractionOptions { ExtractFullPath = true, Overwrite = true });
-                }
-            }
-        }
-
         public string HeaderText
         {
             get { return _headerText; }
@@ -125,5 +55,127 @@ namespace Lyudmila.Client.Windows
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void startDownload(string url)
+        {
+            Task.Delay(1000);
+            if(!Directory.Exists("DL"))
+            {
+                Directory.CreateDirectory("DL");
+            }
+
+            var thread = new Thread(() =>
+            {
+                var client = new WebClient();
+                client.DownloadProgressChanged += client_DownloadProgressChanged;
+                client.DownloadFileCompleted += client_DownloadFileCompleted;
+                client.DownloadFileAsync(new Uri(url), Path.Combine(Environment.CurrentDirectory, "DL", url.Split('/').Last()));
+            });
+            progressBar.IsIndeterminate = false;
+            thread.Start();
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Dispatcher.Invoke((MethodInvoker) delegate
+            {
+                var bytesIn = double.Parse(e.BytesReceived.ToString());
+                var totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                var percentage = bytesIn / totalBytes * 100;
+
+                progressBar.Value = int.Parse(Math.Truncate(percentage).ToString(CultureInfo.InvariantCulture));
+            });
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e) => new Thread(Extract).Start();
+
+        private void Extract()
+        {
+            Dispatcher.Invoke(() => { progressBar.IsIndeterminate = true; });
+
+            if(!Directory.Exists("Jeux"))
+            {
+                Directory.CreateDirectory("Jeux");
+            }
+
+            ExtractCmd(Path.Combine(Environment.CurrentDirectory, "DL", $"{_gameName}.zip"), Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName));
+
+            switch(_gameName)
+            {
+                case "AoE2HD":
+                    Settings.Default.AoE2HD_Installed = true;
+                    Settings.Default.AoE2HD_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "BF3":
+                    Settings.Default.BF3_Installed = true;
+                    Settings.Default.BF3_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "Blur":
+                    Settings.Default.Blur_Installed = true;
+                    Settings.Default.Blur_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "CoD2":
+                    Settings.Default.CoD2_Installed = true;
+                    Settings.Default.CoD2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "CoD4":
+                    Settings.Default.CoD4_Installed = true;
+                    Settings.Default.CoD4_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "CoD5":
+                    Settings.Default.CoD5_Installed = true;
+                    Settings.Default.CoD5_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "CSGO":
+                    Settings.Default.CSGO_Installed = true;
+                    Settings.Default.CSGO_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "DoTA2":
+                    Settings.Default.DoTA2_Installed = true;
+                    Settings.Default.DoTA2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "DoDS":
+                    Settings.Default.DoDS_Installed = true;
+                    Settings.Default.DoDS_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "F2":
+                    Settings.Default.F2_Installed = true;
+                    Settings.Default.F2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "L4D2":
+                    Settings.Default.L4D2_Installed = true;
+                    Settings.Default.L4D2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "SC2":
+                    Settings.Default.SC2_Installed = true;
+                    Settings.Default.SC2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "SWJK2":
+                    Settings.Default.SWJK2_Installed = true;
+                    Settings.Default.SWJK2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "TF2":
+                    Settings.Default.TF2_Installed = true;
+                    Settings.Default.TF2_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+                case "UT3":
+                    Settings.Default.UT3_Installed = true;
+                    Settings.Default.UT3_Location = Path.Combine(Environment.CurrentDirectory, "Jeux", _gameName);
+                    break;
+            }
+
+            Dispatcher.Invoke(Close);
+        }
+
+        private static void ExtractCmd(string zipPath, string extractPath)
+        {
+            using(var archive = ZipArchive.Open(zipPath))
+            {
+                foreach(var entry in archive.Entries.Where(entry => !entry.IsDirectory))
+                {
+                    entry.WriteToDirectory(extractPath, new ExtractionOptions {ExtractFullPath = true, Overwrite = true});
+                }
+            }
+        }
     }
 }
