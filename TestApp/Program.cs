424022b94f7b7902cd6d898a8f2a866ae3a7bfd6
LeafyDev <will09600@gmail.com>
2016-10-26 02:06:52 +0200

    Tests

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -3,6 +3,8 @@
 // -----------------------------------------------------------
 
 using System;
+using System.IO;
+using System.IO.Compression;
 
 namespace TestApp
 {
@@ -10,6 +12,27 @@ namespace TestApp
     {
         private static void Main(string[] args)
         {
+            var zipPath = @"CoD4.zip";
+            var extractPath = Path.Combine(Environment.CurrentDirectory, "Jeux");
+            var dirName = Path.Combine(extractPath, Path.GetFileNameWithoutExtension(zipPath));
+
+            using(var archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach(var entry in archive.Entries)
+                {
+                    if(entry.FullName.EndsWith("/"))
+                    {
+                        ZipFile.ExtractToDirectory(zipPath, extractPath);
+                        break;
+                    }
+                    if(!Directory.Exists(dirName))
+                    {
+                        Directory.CreateDirectory(dirName);
+                        ZipFile.ExtractToDirectory(zipPath, dirName);
+                        break;
+                    }
+                }
+            }
             Console.ReadKey();
         }
     }
